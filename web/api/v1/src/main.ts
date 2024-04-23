13f4a2dddd588b41f16c7f2a44bd4c0fb4e01533
Leandro Melo de Sales <leandromsales@gmail.com>
2024-04-23 05:06:54 -0300

    Reorganizing the api layer

--- web/api/v1/src/main.ts
+++ web/api/v1/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '../../../.env.common' });
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT || 3000);
 }
+console.log(process.env.RPC_URL);
 bootstrap();
