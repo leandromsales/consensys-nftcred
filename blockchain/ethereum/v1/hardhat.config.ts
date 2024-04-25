26e8db2e10791e8b09790421491149f85d2e1eb9
Leandro Melo de Sales <leandromsales@gmail.com>
2024-04-25 04:27:31 -0300

    Use .env in root folder, if exists

--- blockchain/ethereum/v1/hardhat.config.ts
+++ blockchain/ethereum/v1/hardhat.config.ts
@@ -2,7 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import dotenv from "dotenv";
 
-dotenv.config({ path: '../../../.env.common' });
+dotenv.config({ path: '../../../.env' }); // TODO: load only if it exists
 dotenv.config();
 
 // task(
